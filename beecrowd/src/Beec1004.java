62981459c6dc7e74a1b77c676ec0293131dceb3d
Azim <nezdeshniy@protonmail.com>
2025-09-18 18:34:28 +0600

    feat: Beecrowd 1004 ACCEPTED

---
+++ beecrowd/src/Beec1004.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Beec1004 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a  = input.nextInt();
+        int b = input.nextInt();
+
+        int c = a * b;
+
+        System.out.printf("PROD = " + c + "\n");
+    }
+}
