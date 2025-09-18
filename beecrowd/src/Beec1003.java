8125b1e9127b1e9d717caac6f6e8336bd2002e99
Azim <69723927+beif3ng@users.noreply.github.com>
2025-09-18 18:31:48 +0600

    feat: Beecrowd 1003 ACCEPTED

---
+++ beecrowd/src/Beec1003.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Beec1003 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a =  input.nextInt();
+        int b = input.nextInt();
+
+        System.out.printf("SOMA = " + (a+b) + "\n");
+    }
+}
