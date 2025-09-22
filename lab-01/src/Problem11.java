58c86114a0b99171822b340ce7d6f120e40c7884
Azim <69723927+beif3ng@users.noreply.github.com>
2025-09-22 21:57:05 +0600

    feat: Lab 01 Problem 11 SOLVED

---
+++ lab-01/src/Problem11.java
@@ -0,0 +1,16 @@
+public class Problem11 {
+    public static void main(String[] args) {
+        int birth = 7;
+        int death = 13;
+        int immigrant = 45;
+        int population = 312032486;
+        int year = 365 * 24 * 60 * 60;
+
+        double difference = (double) year / birth - (double) year / death + (double) year / immigrant;
+
+        for (int i = 1; i <= 5; i++) {
+            double future = population + i * difference;
+            System.out.printf("Year %d: population is %.0f\n", i, future);
+        }
+    }
+}
