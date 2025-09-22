42f9c1705f7a1cb462b54e86a69eec5e68920e91
Azim <nezdeshniy@protonmail.com>
2025-09-22 14:58:10 +0600

    feat: Lab 01 Problem 10 SOLVED

---
+++ lab-01/src/Problem10.java
@@ -0,0 +1,10 @@
+public class Problem10 {
+    public static void main(String[] args) {
+        double distance = 14 / 1.6;
+        int time = 45 * 60 + 30;
+
+        double speed = distance / time * 100;
+
+        System.out.println("Average speed in miles per hour is " + speed);
+    }
+}
