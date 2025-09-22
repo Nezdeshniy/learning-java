885a64e6735a295f1829d486938ef401c9428fbe
Azim <69723927+beif3ng@users.noreply.github.com>
2025-09-22 14:48:40 +0600

    feat: Lab 01 Problem 08 SOLVED

---
+++ lab-01/src/Problem08.java
@@ -0,0 +1,11 @@
+public class Problem08 {
+    public static void main(String[] args) {
+        double r = 5.5;
+
+        double perimeter = 2 * r * Math.PI;
+        double area = r * r * Math.PI;
+
+        System.out.printf("Perimeter is %f\n", perimeter);
+        System.out.printf("Area is %f\n", area);
+    }
+}
