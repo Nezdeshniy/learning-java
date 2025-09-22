273a4741281591a1431cc9d02215e079850a76d4
Azim <nezdeshniy@protonmail.com>
2025-09-22 14:51:56 +0600

    feat: Lab 01 Problem 09 SOLVED

---
+++ lab-01/src/Problem09.java
@@ -0,0 +1,12 @@
+public class Problem09 {
+    public static void main(String[] args) {
+        double width = 4.5;
+        double height = 7.9;
+
+        double area = width * height;
+        double perimeter =  (width + height) * 2;
+
+        System.out.println("Area of square is: " + area);
+        System.out.println("Perimeter of square is: " + perimeter);
+    }
+}
