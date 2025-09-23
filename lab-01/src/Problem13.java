98106f000fc4ad9683df1fe40031011c2809e18c
Nezdeshniy <nezdeshniy@protonmail.com>
2025-09-23 08:24:36 +0600

    feat: Lab 01 Problem 13 SOLVED

---
+++ lab-01/src/Problem13.java
@@ -0,0 +1,18 @@
+public class Problem13 {
+    public static void main(String[] args) {
+        double a = 3.4;
+        double b = 50.2;
+        double e = 44.5;
+        double c = 2.1;
+        double d = 0.55;
+        double f = 5.9;
+
+        double determinant = a * d - b * c;
+
+        double x = (e * d - b * f) / determinant;
+        double y = (a * f - e * c) / determinant;
+
+        System.out.printf("x = %f\n", x);
+        System.out.printf("y = %f\n", y);
+    }
+}
