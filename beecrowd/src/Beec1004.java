2275251b2c816eb70f3986b97d35d167a1c45020
Azim <nezdeshniy@protonmail.com>
2025-09-28 14:11:44 +0600

    feat: UPDATE Beec1004, Problem10, Problem12

--- beecrowd/src/Beec1004.java
+++ beecrowd/src/Beec1004.java
@@ -4,7 +4,7 @@ public class Beec1004 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int a  = input.nextInt();
+        int a = input.nextInt();
         int b = input.nextInt();
 
         int c = a * b;
