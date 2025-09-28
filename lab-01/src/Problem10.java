510411d7202f566697502ebb384db3475e3d69ab
Azim <69723927+beif3ng@users.noreply.github.com>
2025-09-28 14:11:44 +0600

    feat: UPDATE Beec1004, Problem10, Problem12

--- lab-01/src/Problem10.java
+++ lab-01/src/Problem10.java
@@ -1,9 +1,10 @@
 public class Problem10 {
     public static void main(String[] args) {
-        double distance = 14 / 1.6;
-        int time = 45 * 60 + 30;
+        double distanceMiles = 14 / 1.6;
+        int seconds = 45 * 60 + 30;
+        double hours = seconds / 3600.0;
 
-        double speed = distance / time * 100;
+        double speed = distanceMiles / hours;
 
         System.out.println("Average speed in miles per hour is " + speed);
     }
--- lab-01/src/Problem12.java
+++ lab-01/src/Problem12.java
@@ -1,9 +1,11 @@
 public class Problem12 {
     public static void main(String[] args) {
-        double distance = 24 * 1.6;
-        int time = 1 + 40 * 60 + 35;
+        double distanceKm = 24 * 1.6;
 
-        double speed = distance / time * 100;
+        int seconds = 3600 + 40 * 60 + 35;
+        double hours = seconds / 3600.0;
+
+        double speed = distanceKm / hours;
 
         System.out.println("Average speed in kilometers per hour is " + speed);
     }
