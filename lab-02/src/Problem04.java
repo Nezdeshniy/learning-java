67f288cfcc186141c2aab4151c5c28310a8b2c71
Nezdeshniy <69723927+beif3ng@users.noreply.github.com>
2025-09-30 09:43:55 +0600

    feat: Lab 02 Problem 04 SOLVED

---
+++ lab-02/src/Problem04.java
@@ -0,0 +1,9 @@
+public class Problem04 {
+    public static void main(String[] args) {
+        String name = "Jeanne";
+        String age = "27";
+        String city = "Helsinki";
+
+        System.out.printf("My name is %s, I am %s years old, I live in %s.", name, age, city);
+    }
+}
